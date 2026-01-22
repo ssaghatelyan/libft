6160402519d2cde4a43447bdb3f8f89fb8498562
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-01-22 16:37:48 +0400

    initial commit

---
+++ libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_LIBFT_H
+#define LIBFT_LIBFT_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int    ft_isalpha(int c);
+int    ft_isdigit(int c);
+int    ft_isalnum(int c);
+int    ft_isascii(int c);
+int    ft_isprint(int c);
+int    ft_strlen(char *str);
+
+#endif //LIBFT_LIBFT_H
---
+++ main.c
@@ -0,0 +1,3 @@
+//
+// Created by Admin on 22.01.2026.
+//
