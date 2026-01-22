0374873d46e7667556d33fc0c8f1de07f0471113
ssaghatelyan <saghatelyansusanna06@gmail.com>
2026-01-22 18:27:32 +0400

    initial commit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+
 int  ft_strncmp(char *s1, char *s2, unsigned int n)
 {
     unsigned int  i;
--- libft.h
+++ libft.h
@@ -12,6 +12,7 @@ int    ft_isprint(int c);
 int    ft_toupper(int c);
 int    ft_tolower(int c);
 int    ft_strlen(char *str);
+int    ft_strncmp(char *s1, char *s2, unsigned int n);
 char    *ft_strchr(char *str, int c);
 void    *ft_memset(void *str, int c, size_t n);
 void    ft_bzero(void *str, size_t n);
